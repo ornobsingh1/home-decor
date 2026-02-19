d5d32d1664817e1a81965074ff742ea8fd4649d6
Ornob Singh <ornobsingh14@gmail.com>
2026-02-19 17:16:53 +0600

    adding first commit

---
+++ src/Layouts/MainLayouts.jsx
@@ -0,0 +1,14 @@
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
+
+const MainLayouts = () => {
+  return (
+    <div>
+      <Navbar />
+        
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayouts;
