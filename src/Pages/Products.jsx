29cd49386f252d16eb997b18bd47e5cb16589307
Ornob Singh <ornobsingh14@gmail.com>
2026-02-20 11:04:30 +0600

    adding search functionality

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,19 +1,39 @@
+import { useState } from "react";
 import ProductCard from "../Components/ProductCard";
 import useProducts from "../Hooks/useProducts";
 
 const Products = () => {
   const { products } = useProducts();
+  const [search, setSearch] = useState("");
+  const term = search.trim().toLocaleLowerCase();
+  const searchedProducts = term
+    ? products.filter((product) =>
+        product.name.toLocaleLowerCase().includes(term),
+      )
+    : products;
+
   return (
     <div>
       <div className="flex justify-between items-center my-6">
-        <h1 className="text-2xl font-semibold">All Products</h1>
-        <button to="/products" className="btn btn-outline">
-          Search
-        </button>
+        <h1 className="text-2xl font-semibold">
+          All Products{" "}
+          <span className="text-sm text-gray-500">({searchedProducts.length})</span>
+        </h1>
+
+        <div className="join">
+          <input
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            type="search"
+            className="input join-item"
+            placeholder="Search Products"
+          />
+          {/* <button className="btn join-item rounded-r-full btn-primary">Search</button> */}
+        </div>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 my-4">
-        {products.map((product) => (
+        {searchedProducts.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
